87c4068015f644990c7621dee4c64629b03648eb
David Daxbacher <ddaxbacher.mmt-b2015@fh-salzburg.ac.at>
2016-12-08 12:03:19 +0100

    code fixed js hint

---
+++ app/js/modules/instagram/models/photo.js
@@ -0,0 +1,16 @@
+/*'use strict';
+
+import Model from 'core/model';
+
+module.exports = Model.extend({
+    defaults: {
+        name: 'caesar salad',
+        longitude: 'ravioli',
+        latitude: 'cheesecake',
+        hashtags: 'cheesecake'
+    }
+
+    initialize: function() {
+        console.log('Initializing Twitter Modul...');
+    }
+});*/
