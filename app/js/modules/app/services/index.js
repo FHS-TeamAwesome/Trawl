a1976aefe7deb2768f95fbd20755ad2e6f51c68d
David Daxbacher <ddaxbacher.mmt-b2015@fh-salzburg.ac.at>
2016-12-22 21:00:10 +0100

    fixed aspect ratio in map

--- app/js/modules/app/services/index.js
+++ app/js/modules/app/services/index.js
@@ -3,3 +3,4 @@
 // import services here
 import LinkHandler from 'app/services/link-handler';
 import ProviderManager from 'app/services/provider-manager';
+import Lightbox from 'app/services/lightbox';
