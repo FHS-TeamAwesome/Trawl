7970e3d665ad31affd9643546f9e4a831f91397a
Theresa Höck <thoeck.mmt-b2015@fh-salzburg.ac.at>
2017-01-03 14:40:19 +0100

    get Photos from Facebook

---
+++ app/js/modules/facebook/photos.js
@@ -0,0 +1,302 @@
+/**
+ * Created by theresahock on 02.01.17.
+ */
+
+/**
+ * Created by theresahock on 02.01.17.
+ {
+   "photos": {
+     "data": [
+       {
+         "from": {
+           "name": "Theresa Elisa",
+           "id": "181533942181691"
+         },
+         "images": [
+           {
+             "height": 960,
+             "source": "https://scontent.xx.fbcdn.net/v/t31.0-8/13576627_297988197202931_2409654719974538028_o.jpg?oh=979ada222d49e71d4bb3f3753a60f17b&oe=58E3F486",
+             "width": 1280
+           },
+           {
+             "height": 720,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-9/13606695_297988197202931_2409654719974538028_n.jpg?oh=1f042447d8fd5b66a57a07d800c702a6&oe=58E88916",
+             "width": 960
+           },
+           {
+             "height": 600,
+             "source": "https://scontent.xx.fbcdn.net/v/t31.0-0/p600x600/13576627_297988197202931_2409654719974538028_o.jpg?oh=e2b8e56090f7aa116dbfd3448acc2ef9&oe=58E00A15",
+             "width": 800
+           },
+           {
+             "height": 480,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-0/p480x480/13606695_297988197202931_2409654719974538028_n.jpg?oh=064ef7d513f8b73963b0759a2bbec476&oe=591C3AAD",
+             "width": 640
+           },
+           {
+             "height": 320,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-0/p320x320/13606695_297988197202931_2409654719974538028_n.jpg?oh=3957449579ce51729da1ade35383dc39&oe=591AECF7",
+             "width": 426
+           },
+           {
+             "height": 540,
+             "source": "https://scontent.xx.fbcdn.net/v/t31.0-0/p180x540/13576627_297988197202931_2409654719974538028_o.jpg?oh=5d5b5d3fce374e0957c3560d085575dd&oe=58D69A12",
+             "width": 720
+           },
+           {
+             "height": 130,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-0/p130x130/13606695_297988197202931_2409654719974538028_n.jpg?oh=7f7502e9916439cba0d917a48ac3ae11&oe=591E5B98",
+             "width": 173
+           },
+           {
+             "height": 225,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-0/p75x225/13606695_297988197202931_2409654719974538028_n.jpg?oh=bc186ac3070d7b0368729d0546e96c37&oe=58F1D4EC",
+             "width": 300
+           }
+         ],
+         "place": {
+           "name": "Rocchette-Castiglione Della Pescaia",
+           "location": {
+             "city": "Castiglione della Pescaia",
+             "country": "Italy",
+             "latitude": 42.776665186847,
+             "longitude": 10.797031848944,
+             "street": "strada delle rocchette",
+             "zip": "58043"
+           },
+           "id": "196813390350894"
+         },
+         "id": "297988197202931"
+       },
+       {
+         "from": {
+           "name": "Theresa Elisa",
+           "id": "181533942181691"
+         },
+         "images": [
+           {
+             "height": 960,
+             "source": "https://scontent.xx.fbcdn.net/v/t31.0-8/13558861_297988103869607_5175969208628108540_o.jpg?oh=7fac7d522786bbf3553bb8f3b2ca366c&oe=58EF2423",
+             "width": 1280
+           },
+           {
+             "height": 720,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-9/13619894_297988103869607_5175969208628108540_n.jpg?oh=429855cc852fa4e3ff7d443c7b7aa91d&oe=58ED76A7",
+             "width": 960
+           },
+           {
+             "height": 600,
+             "source": "https://scontent.xx.fbcdn.net/v/t31.0-0/p600x600/13558861_297988103869607_5175969208628108540_o.jpg?oh=8aa4c4463e515c2287de5272b4fe9cd9&oe=5922AFB0",
+             "width": 800
+           },
+           {
+             "height": 480,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-0/p480x480/13619894_297988103869607_5175969208628108540_n.jpg?oh=d317abf3fb275c1a0a09dacfcb96851b&oe=58F0001C",
+             "width": 640
+           },
+           {
+             "height": 320,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-0/p320x320/13619894_297988103869607_5175969208628108540_n.jpg?oh=915996ffcae63515e2def68c33248a83&oe=58EE7A46",
+             "width": 426
+           },
+           {
+             "height": 540,
+             "source": "https://scontent.xx.fbcdn.net/v/t31.0-0/p180x540/13558861_297988103869607_5175969208628108540_o.jpg?oh=780097c3f8634c7232dd0807faed4fc2&oe=58DA18B7",
+             "width": 720
+           },
+           {
+             "height": 130,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-0/p130x130/13619894_297988103869607_5175969208628108540_n.jpg?oh=fc397162e242c801cb86df8ea332c1e7&oe=59213729",
+             "width": 173
+           },
+           {
+             "height": 225,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-0/p75x225/13619894_297988103869607_5175969208628108540_n.jpg?oh=67c60c0b213f34f77d3c8649d2f786fe&oe=591C215D",
+             "width": 300
+           }
+         ],
+         "place": {
+           "name": "Rocchette-Castiglione Della Pescaia",
+           "location": {
+             "city": "Castiglione della Pescaia",
+             "country": "Italy",
+             "latitude": 42.776665186847,
+             "longitude": 10.797031848944,
+             "street": "strada delle rocchette",
+             "zip": "58043"
+           },
+           "id": "196813390350894"
+         },
+         "id": "297988103869607"
+       },
+       {
+         "from": {
+           "name": "Agnes Ramböck",
+           "id": "1099775796706037"
+         },
+         "images": [
+           {
+             "height": 720,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-9/13501667_1371325836217697_7480822127536605597_n.jpg?oh=286a8b588547430373a996018fdc7a4b&oe=58D8BBB6",
+             "width": 960
+           },
+           {
+             "height": 600,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-0/p600x600/13501667_1371325836217697_7480822127536605597_n.jpg?oh=360351d7271ae1a2f3a3cc0a09b6eff3&oe=58EF9484",
+             "width": 800
+           },
+           {
+             "height": 480,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-0/p480x480/13501667_1371325836217697_7480822127536605597_n.jpg?oh=6e73f43a81fbf2eb1e38f1672985cec2&oe=58E6FEAD",
+             "width": 640
+           },
+           {
+             "height": 320,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-0/p320x320/13501667_1371325836217697_7480822127536605597_n.jpg?oh=4b9f476fa9edc23b66fbca09d2807501&oe=58EB69EA",
+             "width": 426
+           },
+           {
+             "height": 540,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-0/p180x540/13501667_1371325836217697_7480822127536605597_n.jpg?oh=638e0b873cf70ccf1b2e96b7a2bdc4fa&oe=58DE87A1",
+             "width": 720
+           },
+           {
+             "height": 130,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-0/p130x130/13501667_1371325836217697_7480822127536605597_n.jpg?oh=c4a1ee25e7d2c0f7b1df30565b990c96&oe=58E31C0D",
+             "width": 173
+           },
+           {
+             "height": 225,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-0/p75x225/13501667_1371325836217697_7480822127536605597_n.jpg?oh=a33f3c6c977c3dd684033bbc45c501fa&oe=58EF76BA",
+             "width": 300
+           }
+         ],
+         "id": "1371325836217697"
+       },
+       {
+         "from": {
+           "name": "Theresa Eibl",
+           "id": "755458681153479"
+         },
+         "images": [
+           {
+             "height": 640,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-9/13226843_1164072826958727_7610511920087324706_n.jpg?oh=d2a274bf1e66e8afce4c4663d32fc9f8&oe=58E701A3",
+             "width": 960
+           },
+           {
+             "height": 600,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-0/p600x600/13226843_1164072826958727_7610511920087324706_n.jpg?oh=88e680f3cff9e0a2ea7764b865367ad1&oe=58EDC091",
+             "width": 900
+           },
+           {
+             "height": 480,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-0/p480x480/13226843_1164072826958727_7610511920087324706_n.jpg?oh=a3942f6d6a9100818be92c2a543b6a3a&oe=58E773B8",
+             "width": 720
+           },
+           {
+             "height": 320,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-0/p320x320/13226843_1164072826958727_7610511920087324706_n.jpg?oh=3c1160dc4e2f1d2312793573b432cd7d&oe=58E739FF",
+             "width": 480
+           },
+           {
+             "height": 540,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-0/p180x540/13226843_1164072826958727_7610511920087324706_n.jpg?oh=b959243e29c88607727cb52bb092d71a&oe=58DDEEB4",
+             "width": 810
+           },
+           {
+             "height": 130,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-0/p130x130/13226843_1164072826958727_7610511920087324706_n.jpg?oh=d5461dc302329d29c854b0591a475ede&oe=58ED1418",
+             "width": 195
+           },
+           {
+             "height": 225,
+             "source": "https://scontent.xx.fbcdn.net/v/t1.0-0/p75x225/13226843_1164072826958727_7610511920087324706_n.jpg?oh=71c133aea80f94213c54196073d0a95d&oe=58EFEFAF",
+             "width": 337
+           }
+         ],
+         "place": {
+           "name": "FRANZ",
+           "location": {
+             "city": "Bergheim",
+             "country": "Austria",
+             "latitude": 47.840345568128,
+             "longitude": 13.022825216026,
+             "street": "Dorfstraße 35",
+             "zip": "5101"
+           },
+           "id": "410445922402270"
+         },
+         "id": "1164072826958727"
+       }
+     ],
+     "paging": {
+       "cursors": {
+         "before": "TWprM09UZAzRNVGszTWpBeU9UTXhPakUwTmpjM09UazVNVGs2TXprME1EZAzVOalF3TmpRM09ETTIZD",
+         "after": "TVRFMk5EQTNNamd5TmprMU9EY3lOem94TkRZAek1qWXpNelkwT2pNNU5EQTRPVFkwTURZAME56ZA3pOZAz09"
+       }
+     }
+   },
+   "id": "181533942181691"
+ }
+ **/
+
+'use strict';
+
+import config from 'config';
+import Collection from 'core/collection';
+import Photo from 'facebook/photo';
+
+const IMAGE_SIZE_IDX = 0;
+
+module.exports = Collection.extend({
+    model: Photo,
+
+    accessToken: null,
+
+    reqDataType: 'jsonp',
+
+    initialize(accessToken) {
+        this.accessToken = accessToken;
+    },
+
+    url() {
+        return config.get('Client.providers.facebook.api') + '/me/photos?fields=images,place&type=uploaded&limit=500&access_token=' + this.accessToken;
+    },
+
+    parse(data) {
+
+        if (!data) return {};
+
+        let photoArr = [];
+
+        for (let image of data.data){
+            let photo;
+            let photoData = {
+                url: image.images[IMAGE_SIZE_IDX].source
+            };
+            if (image.place) {
+                photoData.locationName = image.place.name;
+                photoData.latitude = image.place.location.longitude;
+                photoData.longitude = image.place.location.longitude;
+            }
+
+            photoArr.push(new Photo(photoData));
+
+        }
+
+        return photoArr;
+    },
+
+    getPhotosWithLocation() {
+        let photoArr = [];
+
+        for (let photo of this.models) {
+            if (photo.get('locationName') !== null) {
+                photoArr.push(photo.toJSON());
+            }
+        }
+
+        return photoArr;
+    }
+});
